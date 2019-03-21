a135a9fa1eeca1568eea2f8506defbebd77cd741
ceadarme <45701017+ceadarme@users.noreply.github.com>
2019-03-20 21:49:12 -0500

    eventos.js
    .

---
+++ eventos.js
@@ -0,0 +1,63 @@
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+document.addEventListener("keydown", dibujarTecleado);
+var cuadrito=document.getElementById("area_de_dibujo");
+var papel=cuadrito.getContext("2d");
+var x=150;
+var y=150;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.linewidth=3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujarTecleado(evento)
+{
+var colorcito = "green";
+var movimiento = 1;
+switch (evento.keyCode)
+{
+  case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+  break;
+  case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+  break;
+  case teclas.LEFT:
+    dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+    x = x - movimiento;
+  break;
+  case teclas.RIGHT:
+    dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+    x = x + movimiento;
+  break;
+  }
+}
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>dibujando con flechas</title>
+  </head>
+  <body>
+    <canvas width="300" height="300" id="area_de_dibujo"> </canvas>
+    <p>ueve las flechas del tecleado para dibujar</p>
+    <script src="eventos.js"></script>
+  </body>
+</html>
